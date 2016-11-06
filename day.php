634c12cee1f6413444fa192ceb1cec6f508b5db8
Batue <shaonmd@gmail.com>
2016-11-06 15:20:34 +0600

    first commit

---
+++ day.php
@@ -0,0 +1,36 @@
+<?php
+
+ date_default_timezone_set("Asia/Dhaka"); 
+
+$today = date("D");
+switch($today){
+    case "Mon":
+        echo "Today is Monday.";
+        break;
+    case "Tue":
+        echo "Today is Tuesday.";
+        break;
+    case "Wed":
+        echo "Today is Wednesday.";
+        break;
+    case "Thu":
+        echo "Today is Thursday.";
+        break;
+    case "Fri":
+        echo "Today is Friday.";
+        break;
+    case "Sat":
+        echo "Today is Saturday.";
+        break;
+    case "Sun":
+        echo "Today is Sunday.";
+        break;
+    default:
+        echo "This is Default";
+        break;
+}
+
+?>
+
+
+
